bcab06d082d757a3b2dd61ef2c21d39714812e54
viteshbava <82492234+viteshbava@users.noreply.github.com>
2026-03-26 17:57:16 +1100

    Move contact form in sanity

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -198,6 +198,18 @@ export const structure: StructureResolver = (S) =>
                     .documentId('privacyPolicy')
                     .title('Privacy Policy'),
                 ),
+              // Contact Form Settings - Singleton
+              S.listItem()
+                .id('contactFormSettings')
+                .schemaType('contactFormSettings')
+                .title('Contact Form')
+                .child(
+                  S.editor()
+                    .id('contactFormSettings')
+                    .schemaType('contactFormSettings')
+                    .documentId('contactFormSettings')
+                    .title('Contact Form Settings'),
+                ),
             ]),
         ),
 
@@ -234,18 +246,6 @@ export const structure: StructureResolver = (S) =>
                     .schemaType('siteSettings')
                     .documentId('siteSettings')
                     .title('Site Settings'),
-                ),
-              // Contact Form Settings - Singleton
-              S.listItem()
-                .id('contactFormSettings')
-                .schemaType('contactFormSettings')
-                .title('Contact Form')
-                .child(
-                  S.editor()
-                    .id('contactFormSettings')
-                    .schemaType('contactFormSettings')
-                    .documentId('contactFormSettings')
-                    .title('Contact Form Settings'),
                 ),
             ]),
         ),
